1c8d71deb235a678ea36e7f4627de4dfdc2054b9
J.P Lee <koreanfeel@gmail.com>
2023-06-17 02:19:44 +0900

    생성모델 기능 통합

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 import torchaudio
 from timeit import default_timer as timer
+from infer_util import plot_pianoroll
+from model import drum_generation_model
 from model_zoo import RNNModel_onset, RNNModel_velocity
 from transcription import Dense_onsets, SpecConverter, denoise, high_freq_content, plt_imshow, reducing_time_resolution
 
@@ -36,9 +38,19 @@ velocity_model = RNNModel_velocity(num_nmels=128, hidden_size=128).to(device)
 onset_model.load_state_dict(torch.load('models/onset_model_noQZ.pt'))
 velocity_model.load_state_dict(torch.load('models/velocity_model_noQZ.pt'))
 
+bpm = 120
+style = 'rock'
+style2idx = {'gospel': 0, 'blues': 1,'afrobeat': 2,'jazz': 3,'country': 4,'funk': 5,'rock': 6,
+                      'hiphop': 7,'latin': 8,'dance': 9,'middleeastern': 10,'reggae': 11,'neworleans': 12,
+                      'punk': 13,'pop': 14,'soul': 15,'highlife': 16,'afrocuban': 17}
+
+model = drum_generation_model()
+model.load_state_dict(torch.load('./models/17model_100.pt')) # 아직 가중치가 없어서 추가할 예정입니다. 모델구조와 가중치는 따로 수정할 수 있도록 해주시면 감사하겠습니다! 
+model.eval()
+
 spec_converter = SpecConverter(sr=44100, n_fft=512, hop_length=128, n_mels=128)
 
-def predict(audio_path):
+def transcribe(audio_path, transcription_output):
     start_time = timer()
     audio_path = Path(audio_path)
     sample_first, sr = torchaudio.load(audio_path)
@@ -82,8 +94,26 @@ def predict(audio_path):
     dense_image = f"transcribed_sample_results/densed_drumroll_{audio_path.stem}.png"
     onset_image = f"transcribed_sample_results/onset_pred_cleaned_{audio_path.stem}.png"
     # return pred_labels_and_probs, pred_time
-    return dense_image, onset_image, pred_time
+    transcription_output = densed_drumroll[:,:64]
+    
+    return dense_image, onset_image, pred_time, transcription_output
 
+def generate(x):
+    style_idx = torch.tensor([style2idx[style]], dtype=torch.long)
+    x = torch.from_numpy(x).unsqueeze(0)
+    
+    with torch.no_grad():
+        x_reshape = x.permute(0, 2, 1)
+        vel_outputs, onset_outputs = model(x = x_reshape, style_idx= style_idx, teacher_forcing_ratio = 0.0) 
+        # input이 들어와야합니다. input은 저희 경우 [1, 64, 4]입니다 그래서 아마 지우 형꺼는 [1, 4 ,64라 permute 해야합니다.]
+
+    onset_pred = (onset_outputs > 0.5).float()
+    
+    saved_image = 'transcribed_sample_results/das.png'
+    plot_pianoroll(x_reshape.permute(0,2,1), vel_outputs * onset_pred, out_file_pth=saved_image) 
+    
+    return saved_image
+    
 parser = argparse.ArgumentParser()
 parser.add_argument('--share', type=str2bool, default='False')
 args = parser.parse_args()
@@ -102,7 +132,15 @@ with gr.Blocks() as demo:
     
     time_label = gr.Number(label="Prediction time (s)")
     gr.Markdown("""## Generation""")
+    generate_button = gr.Button(value="Generate")
+    generated_image = gr.Image(label="generated_drumroll")
     
-    run_button.click(fn=predict, inputs=input_audio, outputs=[dense_image, onset_image, time_label])
+    transcription_output = gr.State()
+    
+    #
+    # 버튼 클릭 이벤트
+    #
+    run_button.click(fn=transcribe, inputs=[input_audio, transcription_output], outputs=[dense_image, onset_image, time_label, transcription_output])
+    generate_button.click(fn=generate, inputs=transcription_output, outputs=generated_image)
 
     demo.launch(debug=False, share=args.share)
